7d864971827b1a2ece0843b7f1196030ae57e6d7
Lee <leepotter010@gmail.com>
2022-12-01 19:12:29 +1000

    Initial spring.io code setup

---
+++ src/main/java/com/jolt/stompy/StompyApplication.java
@@ -0,0 +1,13 @@
+package com.jolt.stompy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StompyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StompyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jolt/stompy/StompyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jolt.stompy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StompyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
